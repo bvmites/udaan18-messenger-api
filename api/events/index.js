0e5ba6fb424a326ce3d2c426709a66914ec3e134
Jay Movaliya <jaymovaliya786@gmail.com>
2018-03-01 18:26:59 +0530

    updated participants data

--- api/events/index.js
+++ api/events/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const eventSchema = require('../../schema/event.json');
+const participant = require('../../schema/participant.json');
 const Validator = require('jsonschema').Validator;
 const validator = new Validator();
 module.exports = (db)=>{
@@ -17,5 +18,18 @@ module.exports = (db)=>{
         } catch(e){
             res.status(500).json({message:'event not found'});
     }
-    })
+    });
+    router.get('/:eventId',async(req,res)=>{
+        try{
+            const participant = await Participant.get(req.params.eventId);
+            if(participant!=null){
+                res.status(200).json(participant);
+            }
+            else{
+                res.status(404).json({message:'no participants for this event'})
+            }}
+            catch(e){
+                res.status(401).json({message:'Unauthorized, please check your login'})
+        }
+    });
 }
