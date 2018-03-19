584daaec99ecdc5a275e7c7886b80c67e6afa177
jaymovaliya <jaymovaliya786@gmail.com>
2018-03-19 17:34:02 +0530

    tested sockets

--- api/events/index.js
+++ api/events/index.js
@@ -13,7 +13,6 @@ module.exports = (db, io) => {
         try {
             const event = await Event.get(req.params.id);
             console.log(event);
-            console.log(event.currentRound);
             if (event != null) {
                 res.status(200).json(event);
             }
@@ -45,19 +44,6 @@ module.exports = (db, io) => {
 
         } catch (e) {
             res.status(500).json({message: 'internal server error'});
-        }
-    });
-    router.put('/textlocal', async (req, res) => {
-        try {
-            const {number, status, customID, datetime} = req.body;
-            const getReceipt = await Participant.receipt(number, status, customID, datetime);
-            const updateStatus = await Participant.updateStatus(number, customID, status);
-            socketClientList.forEach(function (socket) {
-                socket.emit('receipt', req.body);
-            });
-            res.status(200).json({message: "got receipt and updated status"});
-        } catch (e) {
-            console.log(e);
         }
     });
     /*router.post('/:eventId/participations', async (req,res) => {
--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const dotenv = require('dotenv');
 
 const users = require('./api/users');
 const events = require('./api/events');
+const textlocal = require('./api/textlocal');
 const auth = require('./middleware/auth');
-const port = process.env.port || 3000;
+const port = process.env.port || 4000;
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
@@ -20,10 +21,13 @@ dotenv.config();
         const client = await MongoClient.connect(process.env.DB);
         console.log('Connectd to database.');
         const db = client.db('udaan18');
-
         const db2 = client.db('users');
-        app.use('/events', auth, events(db,io));
+        app.use('/events', auth, events(db, io));
         app.use('/users', users(db2));
+        app.use('/textlocal', textlocal(db, io));
+        app.get('/', function (req, res) {
+            res.sendFile(__dirname + '/index.html');
+        });
         app.use(function (req, res, next) {
             let err = new Error('Not Found');
             err.status = 404;
@@ -33,7 +37,7 @@ dotenv.config();
         app.use(function (err, req, res, next) {
             res.locals.message = err.message;
             res.locals.error = req.app.get('env') === 'development' ? err : {};
-            res.status(err.status || 500);
+            res.status(err.status || 500).json({message: err.message});
         });
     } catch (e) {
         console.log('Cannot connect');
